aee855b45b974f3dd6acbc717e43c402b0dbe617
Sander Zeeman <szeeman2000@gmail.com>
2022-04-21 23:07:24 +0200

    Set up template

--- Data/CSV/src/main.rs
+++ Data/CSV/src/main.rs
@@ -9,4 +9,19 @@ fn main() {
     }
     let command = args.get(1).unwrap();
     let filename = args.get(2).unwrap();
+
+    let _csv_string = std::fs::read_to_string(filename);
+
+    match command.as_str() {
+        "decode" => {
+            todo!();
+        }
+        "encode" => {
+            todo!();
+        }
+        _ => {
+            println!("Unknown command: {:?}", command);
+            return
+        }
+    }
 }
